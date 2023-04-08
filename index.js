ba0974229be853d5edd03a80db8f4c039da21b1b
AdilakshmiBonala <adilakshmi.bonala@techgrit.com>
2023-04-08 11:58:30 +0530

    Add practice files

---
+++ index.js
@@ -0,0 +1,8 @@
+const rootElement = document.getElementById("root");
+const element = (
+  <div>
+    <h1 className="greeting"> Hello !</h1>
+    <p>Good to see you </p>
+  </div>
+);
+ReactDOM.render(element, rootElement);
